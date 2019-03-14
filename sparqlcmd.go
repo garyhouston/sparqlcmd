7a23ec78ea8b1d4a4a16df3c71a2063b3d191c7a
Gary Houston <ghouston@arglist.com>
2019-03-15 09:38:51 +1100

    Print results directly.

--- sparqlcmd.go
+++ sparqlcmd.go
@@ -9,53 +9,52 @@ import (
 	"os"
 )
 
-func process(query string) ([][]string, error) {
+func process(query string) error {
 	endpoint := "https://query.wikidata.org/bigdata/namespace/wdq/sparql?"
 	full := endpoint + "query=" + url.QueryEscape(query) + "&format=json"
 	req, err := http.NewRequest("POST", full, nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	req.Header.Add("User-Agent", "sparqlcmd")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	json, err := jason.NewObjectFromBytes(body)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	vars, err := json.GetStringArray("head", "vars")
 	if err != nil {
-		return nil, err
+		return err
 	}
 	bindings, err := json.GetObjectArray("results", "bindings")
 	if err != nil {
-		return nil, err
-	}
-	result := make([][]string, len(vars))
-	for i := range vars {
-		result[i] = make([]string, 0, 100)
+		return err
 	}
 	for i := range bindings {
 		obj, err := bindings[i].Object()
 		if err != nil {
-			return nil, err
+			return err
 		}
+		sp := ""
 		for j := range vars {
 			val, err := obj.GetString(vars[j], "value")
 			if err != nil {
-				return nil, err
+				return err
 			}
-			result[j] = append(result[j], val)
+			fmt.Printf("%s%q", sp, val)
+			sp = " "
 		}
+		fmt.Println()
 	}
-	return result, nil
+	return nil
 }
 
 func main() {
@@ -63,16 +62,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	result, err := process(string(query))
+	err = process(string(query))
 	if err != nil {
 		panic(err)
-	}
-	for i := range result[0] {
-		sp := ""
-		for j := range result {
-			fmt.Printf("%s%q", sp, result[j][i])
-			sp = " "
-		}
-		fmt.Println()
 	}
 }
